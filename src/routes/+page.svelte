66ec73e10986d5a3233ec464e101b139aedfa52b
nick <nla@melix.net>
2023-07-12 22:51:15 +0200

    chore: wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	@use '$lib/ui/styles.scss';
+
+	h1 {
+		color: styles.$color-primary-5;
+	}
+</style>
